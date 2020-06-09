2f7ff2a6a112d16ccbf252b196537f199ad23fd0
Carlos <carlos.ruiz.cues@gmail.com>
2020-06-09 17:11:15 +0200

    Initial commit

---
+++ LectorRss/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LectorRss
+//
+//  Created by MIGUEL PEREZ SUBIAS on 09/06/2020.
+//  Copyright © 2020 Carlos Ruiz Cuesta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
