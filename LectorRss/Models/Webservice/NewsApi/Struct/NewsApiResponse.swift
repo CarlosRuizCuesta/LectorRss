4014c412ba38ade22584a95ead9f6ede577bcc59
Carlos <carlos.ruiz.cues@gmail.com>
2020-06-09 17:54:27 +0200

    Creation of Custom Types to obtain JSON data from the service

---
+++ LectorRss/Models/Webservice/NewsApi/Struct/NewsApiResponse.swift
@@ -0,0 +1,16 @@
+//
+//  NewsApiResponse.swift
+//  LectorRss
+//
+//  Created by MIGUEL PEREZ SUBIAS on 09/06/2020.
+//  Copyright © 2020 Carlos Ruiz Cuesta. All rights reserved.
+//
+
+import Foundation
+
+class NewsApiResponse : Codable {
+    
+    var status : String!
+    var totalResults : Int!
+}
+
