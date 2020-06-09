5fd385c9a7a494f77e59b1586ef0a2e60a0ae12d
Carlos <carlos.ruiz.cues@gmail.com>
2020-06-09 18:42:34 +0200

    Creation of protocols for the response of the webservice

---
+++ LectorRss/Models/Webservice/NewsApi/Protocols/NewsApiProtocols.swift
@@ -0,0 +1,15 @@
+//
+//  NewsapiProtocols.swift
+//  LectorRss
+//
+//  Created by MIGUEL PEREZ SUBIAS on 09/06/2020.
+//  Copyright © 2020 Carlos Ruiz Cuesta. All rights reserved.
+//
+
+import Foundation
+
+
+protocol NewsApiProtocols {
+    func newsApiResult(data : NewsApiResponse)
+    func newsApiError(error : NewsApiErrorsEnum)
+}
