68391769d1ab610fb59e5cac0f6160ec72ceadac
Carlos <carlos.ruiz.cues@gmail.com>
2020-06-09 18:41:53 +0200

    Creation of enum for the management of errors in the webservice. Refactoring of NewsApi.swift

---
+++ LectorRss/Models/Webservice/NewsApi/Models/NewsApiErrors.swift
@@ -0,0 +1,16 @@
+//
+//  NewsApiErros.swift
+//  LectorRss
+//
+//  Created by MIGUEL PEREZ SUBIAS on 09/06/2020.
+//  Copyright © 2020 Carlos Ruiz Cuesta. All rights reserved.
+//
+
+import Foundation
+
+
+public enum NewsApiErrorsEnum : String {
+    case networking = "networking error";
+    case http = "http error";
+    case data = "error decoding JSON";
+}
