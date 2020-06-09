817e6aaffb5feefdf41c1b29f964ca1f98bcca48
Carlos <carlos.ruiz.cues@gmail.com>
2020-06-09 17:33:15 +0200

    Creation of class newsApi to manage the call to the service newsapi.org

---
+++ LectorRss/Models/Webservice/NewsApi/NewsApi.swift
@@ -0,0 +1,32 @@
+//
+//  NewsApi.swift
+//  LectorRss
+//
+//  Created by MIGUEL PEREZ SUBIAS on 09/06/2020.
+//  Copyright © 2020 Carlos Ruiz Cuesta. All rights reserved.
+//
+
+import Foundation
+
+class NewsApi {
+    static var PARAM_SEARCH : String = "q"
+    static var PARAM_SORT : String = "sortBy"
+    static var PARAM_API : String = "apiKey"
+    static var URL : String = "https://newsapi.org/v2/everything"
+    static var API = "619e89b805954fcf8cbbe753b221bcf2"
+    
+    var search : String = "ios"
+    var sortBy : String = "Date"
+    var url = URLComponents(string: URL)
+    var parameters: [String: Any]!
+    
+    
+    init() {
+        
+        url?.queryItems = [
+            URLQueryItem(name: NewsApi.PARAM_SEARCH, value: search),
+            URLQueryItem(name: NewsApi.PARAM_SORT, value: sortBy),
+            URLQueryItem(name: NewsApi.PARAM_API, value: NewsApi.API)
+        ]
+    }
+}
