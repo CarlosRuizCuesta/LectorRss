89ee7be383407f03e8c653b1cefe10a3527431f4
Carlos <carlos.ruiz.cues@gmail.com>
2020-06-10 21:37:53 +0200

    Search news in english

--- LectorRss/Models/Webservice/NewsApi/Models/NewsApi.swift
+++ LectorRss/Models/Webservice/NewsApi/Models/NewsApi.swift
@@ -8,14 +8,17 @@
 
 import Foundation
 
+// https://newsapi.org/
 class NewsApi {
+    static var PARAM_LANG : String = "language"
     static var PARAM_SEARCH : String = "q"
     static var PARAM_SORT : String = "sortBy"
     static var PARAM_API : String = "apiKey"
     static var URL : String = "https://newsapi.org/v2/everything"
     static var API = "619e89b805954fcf8cbbe753b221bcf2"
     
-    var search : String = "ios"
+    var lang : String = "en"
+    var search : String = "news"
     var sortBy : String = "publishedAt"
     var url = URLComponents(string: URL)
     var parameters: [String: Any]!
@@ -26,6 +29,7 @@ class NewsApi {
         url?.queryItems = [
             URLQueryItem(name: NewsApi.PARAM_SEARCH, value: search),
             URLQueryItem(name: NewsApi.PARAM_SORT, value: sortBy),
+            URLQueryItem(name: NewsApi.PARAM_LANG, value: lang),
             URLQueryItem(name: NewsApi.PARAM_API, value: NewsApi.API)
         ]
     }
